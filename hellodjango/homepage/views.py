1129f665fcf27a93c1153ab758e3d9a1665428af
Iliyan0411 <ilian.dd00@gmail.com>
2021-09-23 21:07:22 +0300

    Using variables.

--- hellodjango/homepage/views.py
+++ hellodjango/homepage/views.py
@@ -1,4 +1,10 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render
 
-class HomepageView(TemplateView):
-    template_name = 'index.html'
+
+def page(request):
+    first_name = "Iliyan"
+    last_name = "Dimitrov"
+    age = 20
+    parents = ["Dimitar", "Aneta"]
+
+    return render(request, 'index.html', {"first": first_name, "last": last_name, "age": age, "parents": parents})
