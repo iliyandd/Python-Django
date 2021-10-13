61f44e348af80c70a4752059c847c9800d5bd435
Iliyan0411 <ilian.dd00@gmail.com>
2021-10-13 14:25:51 +0300

    CV app skeleton

---
+++ MyCompany/CV/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+
+"""
+Blank form:
+1. First name
+2. Last name
+3. Email
+4. Phone number
+"""
+
+"""
+CV form:
+1. Personal URL
+2. Position
+3. Technologies
+4. Languages
+5. Motivation
+"""
+
+"""
+Report form:
+1. Issue
+"""
+
+"""
+Candidate model:
+1. First name
+2. Last name
+3. Email
+4. Phone number
+"""
