a8618a73fb198115903df41604827b0a062cef6c
Iliyan0411 <ilian.dd00@gmail.com>
2021-11-19 17:58:27 +0200

    Building of Note form and more.

---
+++ NoteMaster/Notes/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from .models import Note
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2', 'notes']
+
+
+class NoteForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(NoteForm, self).__init__(*args, **kwargs)
+        self.fields['content'].required = False
+
+    class Meta:
+        model = Note
+        fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'id': 'note_title',
+                'placeholder': 'Title'
+            }),
+            'content': forms.Textarea(attrs={
+                'id': 'note_content',
+                'rows': '10',
+                'cols': '40'
+            }) 
+        }
