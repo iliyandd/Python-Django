2a29e2978297a665c8b037d2fa0e9f0697d1fd65
Iliyan0411 <ilian.dd00@gmail.com>
2021-12-11 17:12:16 +0200

    Get posts with AJAX call.

--- CRUD/posts/static/posts/main.js
+++ CRUD/posts/static/posts/main.js
@@ -1,13 +1,30 @@
 const helloWorldBox = document.getElementById("hello-world");
+const postsBox = document.getElementById("posts-box");
 
 // helloWorldBox.textContent = "Hello world";
 // helloWorldBox.innerHTML = "Hello <b>world</b>";
 
 $.ajax({
+    type: "GET",
+    url: "/data/",
+    success: function(response){
+        const data = response.data;
+        console.log(data);
+
+        data.forEach(el => {
+            postsBox.innerHTML += `${el.title} - <b>${el.body}</b><br>`;
+        });
+    },
+    error: function(error){
+        console.log(error);
+    }
+})
+
+
+$.ajax({
     type: 'GET',
     url: '/hello-world/',
     success: function (response) {
-        // console.log('success', response);
         console.log('success', response.text);
         helloWorldBox.textContent = response.text;
     },
@@ -17,6 +34,8 @@ $.ajax({
 })
 
 
+
+// Fetch data every 5 seconds
 // function f(){
 //     $.ajax({
 //         type: 'GET',
--- CRUD/posts/views.py
+++ CRUD/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse
+# from django.core import serializers
 
 # Create your views here.
 def post_list_and_create(request):
@@ -9,9 +10,21 @@ def post_list_and_create(request):
     return render(request, 'posts/main.html', {'qs': qs})
 
 
+def load_post_data_view(request):
+    qs = Post.objects.all()
+    # data = serializers.serialize('json', qs)
+
+    data = []
+    for obj in qs:
+        post = {
+            "id": obj.id,
+            "title": obj.title,
+            "body": obj.body
+        }
+        data.append(post)
+
+    return JsonResponse({"data": data})
+
+
 def hello_world_view(request):
-    # first_title = Post.objects.last().title
-
-    # return JsonResponse({'text': first_title})
-
     return JsonResponse({'text': 'Hello world'})
