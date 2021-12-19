b2e31ff8af48f43982b020d7280d60b0b08e0909
Iliyan0411 <ilian.dd00@gmail.com>
2021-12-19 18:52:26 +0200

    Some CSS

--- NoteMaster/static/js/home.js
+++ NoteMaster/static/js/home.js
@@ -1,53 +1,47 @@
+const noteBox = document.getElementById("display_notes");
+const contentBox = document.getElementById("display_content");
 
-class NotesController
-{
-    constructor(){
-        this.noteBox = document.getElementById("display_notes");
-        this.contentBox = document.getElementById("display_content");
-    }
 
-    displayNotes(){
-        $.ajax({
-            type: "GET",
-            url: "/display-notes/",
-            success: (response) => {
-                const notes = response.notes;
-                
-                this.noteBox.innerHTML = "";
-                notes.forEach(note => {
-                    this.noteBox.innerHTML += `
-                        <span class="note-row">
-                            <span class="note">${note.title}</span>
-                        </span>
-                        <button>Edit</button>
-                        <button>Delete</button><br><br><br>
-                    `;
-                });
-    
-                this.prepareNotesContent(notes);
-            },
-            error: (error) => {
-                console.log("error", error);
-            }
-        })
-    }
+function prepareNotesContent(notes){
+    var buttons = document.getElementsByClassName("note-row");
 
-    prepareNotesContent(notes){
-        var buttons = document.getElementsByClassName("note-row");
-    
-        for(let i = 0;  i < buttons.length; i++){
-            buttons[i].onclick = () => {
-                const title = `~~~ ${notes[i].title} ~~~\n\n`;
-                this.contentBox.innerHTML = title;
-                this.contentBox.innerHTML += notes[i].content;
-            }
+    for(let i = 0;  i < buttons.length; i++){
+        buttons[i].onclick = () => {
+            const title = `~~~ ${notes[i].title} ~~~\n\n`;
+            contentBox.innerHTML = title;
+            contentBox.innerHTML += notes[i].content;
         }
     }
 }
 
+function displayNotes(){
+    $.ajax({
+        type: "GET",
+        url: "/display-notes/",
+        success: (response) => {
+            console.log("success", response);
+            const notes = response.notes;
+            
+            noteBox.innerHTML = "";
+            notes.forEach(note => {
+                noteBox.innerHTML += `
+                    <span class="note-row">
+                        <span class="note">${note.title}</span>
+                    </span>
+                    <button class="note-button">Edit</button>
+                    <button class="note-button">Delete</button><br><br><br>
+                `;
+            });
+
+            prepareNotesContent(notes);
+        },
+        error: (error) => {
+            console.log("error", error);
+        }
+    })
+}
+
 
 // function calls...
-const noteController = new NotesController();
-
-noteController.displayNotes();
-setInterval(noteController.displayNotes, 5000);
+displayNotes();
+setInterval(displayNotes, 5000);
