f807bee72482544211b58912b237e02532cc851f
Iliyan0411 <ilian.dd00@gmail.com>
2021-12-21 13:32:28 +0200

    Delete option

--- NoteMaster/static/js/home.js
+++ NoteMaster/static/js/home.js
@@ -1,18 +1,49 @@
 const noteBox = document.getElementById("display_notes");
 const contentBox = document.getElementById("display_content");
+let lastClickedNoteRow = -1;
 
 
-function prepareNotesContent(notes){
-    var buttons = document.getElementsByClassName("note-row");
+function loadNotesContent(notes){
+    const noteRows = document.getElementsByClassName("note-row");
 
-    for(let i = 0;  i < buttons.length; i++){
-        buttons[i].onclick = () => {
+    for(let i = 0;  i < noteRows.length; i++){
+        noteRows[i].addEventListener("click", function(){
             const title = `~~~ ${notes[i].title} ~~~\n\n`;
             contentBox.innerHTML = title;
             contentBox.innerHTML += notes[i].content;
-        }
+
+            lastClickedNoteRow = notes[i].id;
+        });
     }
 }
+
+
+function loadDeleteButtons(notes){
+    const deleteButtons = document.querySelectorAll(".del-button");
+
+    for(let i = 0; i < deleteButtons.length; i++){
+
+        deleteButtons[i].addEventListener("click", function(){
+            console.log("Here 2");
+            $.ajax({
+                type: "POST",
+                url: "/delete-note/",
+                data: {
+                    note_id: `${notes[i].id}`
+                },
+                success: function(){
+                    if(lastClickedNoteRow == notes[i].id){
+                        contentBox.innerHTML = "";
+                    }
+                },
+                error: function(){
+                    alert("Error with deletion of the note!");
+                }
+            });
+        });
+    }
+}
+
 
 function displayNotes(){
     $.ajax({
@@ -28,20 +59,21 @@ function displayNotes(){
                     <span class="note-row">
                         <span class="note">${note.title}</span>
                     </span>
-                    <button class="note-button">Edit</button>
-                    <button class="note-button">Delete</button><br><br><br>
+                    <button class="note-button edit-button">Edit</button>
+                    <button class="note-button del-button">Delete</button><br><br><br>
                 `;
             });
 
-            prepareNotesContent(notes);
+            loadNotesContent(notes);
+            loadDeleteButtons(notes);
         },
         error: (error) => {
             console.log("error", error);
         }
-    })
+    });
 }
 
 
 // function calls...
 displayNotes();
-setInterval(displayNotes, 5000);
+setInterval(displayNotes, 2000);
